3e7a29cb6e8e9f474b1b16497b70ca6bc9cf054a
Gustavo Borges <gustavo.borges@catskillet.com>
2021-10-22 10:47:27 -0300

    feat: metodos de deposito e middleware para verificar se existe conta

--- src/index.js
+++ src/index.js
@@ -6,6 +6,18 @@ app.use(express.json());
 
 const customers = [];
 
+// Middleware
+function verifyIfExistsAccountCPF(request, response, next) {
+  const { cpf } = request.headers;
+  const customer = customers.find(customer => customer.cpf === cpf);
+
+  if (!customer) {
+    return response.status(400).json({ error: "Customer not found!" })
+  }
+  request.customer = customer;
+  return next();
+}
+
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
 
@@ -20,6 +32,29 @@ app.post("/account", (request, response) => {
   customers.push({
     cpf, name, id: uuidv4(), statement: []
   });
+  return response.status(201).send();
+});
+
+//app.use(verifyIfExistsAccountCPF);
+
+app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+  return response.status(200).json(customer.statement);
+});
+
+app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
+  const { description, amount } = request.body;
+  const { customer } = request;
+
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: "credit"
+  }
+
+  customer.statement.push(statementOperation);
+
   return response.status(201).send();
 });
 
